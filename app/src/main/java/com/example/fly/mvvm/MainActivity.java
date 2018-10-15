a8f063928cb91849cf4cce8f711af34c10ed5c63
wangfei <gaodun123com>
2018-10-15 11:00:08 +0800

    获取手机信息

--- app/src/main/java/com/example/fly/mvvm/MainActivity.java
+++ app/src/main/java/com/example/fly/mvvm/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fly.mvvm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showSystemParameter();
+    }
+
+    private void showSystemParameter() {
+        String TAG = "系统参数：";
+        Log.e(TAG, "手机厂商：" + SystemUtil.getDeviceBrand());
+        Log.e(TAG, "手机型号：" + SystemUtil.getSystemModel());
+        Log.e(TAG, "手机当前系统语言：" + SystemUtil.getSystemLanguage());
+        Log.e(TAG, "Android系统版本号：" + SystemUtil.getSystemVersion());
+        Log.e(TAG, "手机IMEI：" + SystemUtil.getIMEI(getApplicationContext()));
     }
 }
