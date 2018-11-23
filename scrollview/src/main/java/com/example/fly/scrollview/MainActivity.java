c72b0cfd0d348771f0da36c48ed5febc82f0c4d0
wangfei <gaodun123com>
2018-11-23 11:29:19 +0800

    RecyclerView刷新的封装

--- scrollview/src/main/java/com/example/fly/scrollview/MainActivity.java
+++ scrollview/src/main/java/com/example/fly/scrollview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fly.scrollview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String reservation = "null";
+        if (!TextUtils.isEmpty(reservation) && !"null".equals(reservation)) {
+//        if (!TextUtils.isEmpty(reservation)) {
+            Log.e("MainActivity",reservation);
+        }else{
+            Log.e("MainActivity","----- else ----");
+        }
     }
 }
